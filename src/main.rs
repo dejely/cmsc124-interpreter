ed38e5d0e01f4082405b7ffcb236c28f3db4481a
Dejel Cyrus De Asis <38832544+dejely@users.noreply.github.com>
2026-09-08 01:59:09 +0800

    meta: removed test from main

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,4 @@ use scanner::Scanner;
 
 fn main() {
     println!("Hello, JM & Dejel!");
-    let mut scanner = Scanner::new("(){},.-+;/*[]!=><".to_string());
-    let tokens = scanner.scan_tokens();
-    println!("{tokens:#?}");
 }
