6c0e5056a96eb4c47a9367dda1a826cc556defaf
Dejel Cyrus De Asis <38832544+dejely@users.noreply.github.com>
2026-09-08 01:56:13 +0800

    meta: add basic tests for debugging

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 mod scanner;
 mod token;
 
+use scanner::Scanner;
+
 fn main() {
     println!("Hello, JM & Dejel!");
+    let mut scanner = Scanner::new("(){},.-+;/*[]!=><".to_string());
+    let tokens = scanner.scan_tokens();
+    println!("{tokens:#?}");
 }
